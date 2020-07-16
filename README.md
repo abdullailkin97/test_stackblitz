43524c99ab17e25fc554a348ead5482ad6bdce58
abdullayevilkin <abdullayevilkin1997@gmail.com>
2020-07-17 00:14:05 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# testilkin
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/testilkin)
---
+++ index.js
@@ -0,0 +1,8 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starterf Test 2.. :)</h1>
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/71vpN1h03PL._AC_SY679._SX._UX._SY._UY_.jpg" alt="Girl in a jacket" width="60" height="80"> 
+`;
